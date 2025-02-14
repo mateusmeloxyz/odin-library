ee1b91ddffbd7d5d05430358c92236d6c0df57cf
mateusmeloxyz <melo.mateus@proton.me>
2025-02-14 11:58:25 -0300

    Create skeleton files

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!")
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${pages} pages, ${this.read ? "read" : "not read yet"}`
+    }
+}
+
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(book1.info())
