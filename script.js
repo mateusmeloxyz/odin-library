fa756a82ab9d04cc3d7712dc06c3481435fcb9bc
Mateus Melo <mateus@mateusmelo.com>
2025-07-22 19:16:14 -0300

    Create ID for book structure

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new'operator to call the constructor");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
