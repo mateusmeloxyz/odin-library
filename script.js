6171d0f3388fd0c4debdc123b3dce4fd46f6c431
Mateus Melo <mateus@mateusmelo.com>
2025-07-22 20:11:12 -0300

    Create addBookToLibrary and displayBooks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new'operator to call the constructor");
@@ -12,6 +14,20 @@ function Book(title, author, pages, read) {
   };
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
 
 console.log(book1.info());
+console.log(book2.info());
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+function displayBooks(library) {
+  library.forEach((book) => console.log(book.info()));
+}
+
+displayBooks(myLibrary);
