54744b44aebe5e8caf31167054570fce104356f4
Mateus Melo <mateus@mateusmelo.com>
2025-07-22 19:11:45 -0300

    Auto-format

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function Book(title, author, pages, read){
-    if (!new.target) {
-        throw Error("You must use the 'new'operator to call the constructor");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${pages} pages, ${this.read ? "read" : "not read yet"}`
-    }
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new'operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${pages} pages, ${this.read ? "read" : "not read yet"}`;
+  };
 }
 
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
-console.log(book1.info())
+console.log(book1.info());
